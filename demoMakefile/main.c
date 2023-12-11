4d2276ed8a32b6ebc98894cf9a453dc222173660
chenyuekang <1207867631@qq.com>
2023-12-11 16:42:44 +0800

    [新增]预处理_汇编_编译原理

---
+++ demoMakefile/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int printString()
+{
+    int ret = 0;
+    printf("printf string\n");
+    return ret;
+}
+
+int main()
+{
+    int num1 = 10;
+    printf("num:%d\n", num1);
+    printString();
+    return 0;
+}
